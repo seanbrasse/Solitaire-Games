fef9e54783a5893a24bbdf2bdaaf93e0fc83db47
Seanb <Seanb@DESKTOP-1V685H7>
2018-09-10 08:58:28 -0400

    I can talk to cows.

--- src/edu/buffalo/cse116/Main.java
+++ src/edu/buffalo/cse116/Main.java
@@ -3,7 +3,7 @@ package edu.buffalo.cse116;
 public class Main {
 
 	public static void main(String [] args) {
-		System.out.println("Yerrrrrrr");
+		System.out.println("moo");
 		//dashdass
 		int a = 7;
 		System.out.println(a+2);

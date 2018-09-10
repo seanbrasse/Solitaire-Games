bc85249438830f801843900dc99837299f9c89a4
Seanb <Seanb@DESKTOP-1V685H7>
2018-09-10 12:10:19 -0400

    Got it

--- src/edu/buffalo/cse116/Main.java
+++ src/edu/buffalo/cse116/Main.java
@@ -4,10 +4,8 @@ public class Main {
 
 	public static void main(String [] args) {
 		System.out.println("moo");
-		//dashdass
-		int a = 7;
-		System.out.println(a+2);
-		
-		
+		char a = '!';
+		System.out.println("I figured out how pushing and pulling works" + a);
+		//(Sean) 
 	}
 }

8edaaaf995450235ee6732e412a77b756aa58782
Willshady <Willshady@10.84.83.67>
2018-09-27 15:23:21 -0400

    Created game class for golf and little spider

---
+++ src/edu/buffalo/cse116/code/GolfGame.java
@@ -0,0 +1,13 @@
+package edu.buffalo.cse116.code;
+
+/**
+ * Class representing a game instance of
+ * golf.
+ * 
+ * @author Willshady
+ *
+ */
+public class GolfGame 
+{
+	
+}
---
+++ src/edu/buffalo/cse116/code/LittleSpiderGame.java
@@ -0,0 +1,13 @@
+package edu.buffalo.cse116.code;
+
+/**
+ * Class representing a game instance of 
+ * little spider.
+ * 
+ * @author Willshady
+ *
+ */
+public class LittleSpiderGame 
+{
+	
+}

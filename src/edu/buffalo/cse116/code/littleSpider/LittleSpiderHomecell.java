6bc2379b280ca285ea5cd2fde8533c0c6db64ae2
Seanb <Seanb@DESKTOP-1V685H7>
2018-09-29 13:23:31 -0400

    Fixed isAddingCardLegal method

--- src/edu/buffalo/cse116/code/littleSpider/LittleSpiderHomecell.java
+++ src/edu/buffalo/cse116/code/littleSpider/LittleSpiderHomecell.java
@@ -6,7 +6,7 @@ import edu.buffalo.cse116.code.Card;
 import edu.buffalo.cse116.code.Homecell;
 
 public class LittleSpiderHomecell extends Homecell {
-	
+
 	/**
 	 * RULES DECLARED ON WEBSITE:
 	 * Once a card is removed, the card following it in
@@ -25,7 +25,7 @@ public class LittleSpiderHomecell extends Homecell {
 			//WE ARE RETURNING THIS IF LEGAL.
 			return this.homecellPile.pop();
 		}
-		
+
 		return null;
 	}
 
@@ -55,38 +55,40 @@ public class LittleSpiderHomecell extends Homecell {
 	 * be added to a homecell pile if it has the identical suit as the top card AND a rank 
 	 * one BELOW the homecell pile's top card. Cards CANNOT wrap when adding to a homecell pile.
 	 */
-	
+
 	@Override
 	public boolean isAddingCardLegal(Card card) {
-		if(this.homecellPile.lastElement().getSuit() == 1  && card.getSuit() == 1 || this.homecellPile.lastElement().getSuit() == 2 && card.getSuit() == 2) {
-			if(card.getRank() - this.homecellPile.lastElement().getRank() == 1) {
-				return true;
-		} else if(this.homecellPile.lastElement().getSuit() == 0  && card.getSuit() == 0 || this.homecellPile.lastElement().getSuit() == 3 && card.getSuit() == 3) {
-			if(card.getRank() - this.homecellPile.lastElement().getRank() == -1){
-				return true;
+		if(this.homecellPile.size() > 0) {
+			if(this.homecellPile.lastElement().getSuit() == 1  && card.getSuit() == 1 || this.homecellPile.lastElement().getSuit() == 2 && card.getSuit() == 2) {
+				if(card.getRank() - this.homecellPile.lastElement().getRank() == 1) {
+					return true;
+				}
+			} else if(this.homecellPile.lastElement().getSuit() == 0  && card.getSuit() == 0 || this.homecellPile.lastElement().getSuit() == 3 && card.getSuit() == 3) {
+				if(card.getRank() - this.homecellPile.lastElement().getRank() == -1){
+					return true;
+				}
 			}
 		}
+		// TODO Auto-generated method stub
+		return false;
 	}
-	// TODO Auto-generated method stub
-	return false;
-}
-	
-//	@Override
-//	public boolean isAddingCardLegal(Card card)
-//	{
-//		//WE ARE CHECKING TOP CARD AND COMPARING IT TO @PARAM CARD TO SEE IF IT WRAPS.
-//		if(homecellPile.peek().canBuild(card) || homecellPile.peek().canWrap(card))
-//		{
-//			//WE ARE CHECKING TOP CARD AND COMPARING IT TO @PARAM CARD TO SEE IF ITS SAME SUIT.
-//			if(homecellPile.peek().isSameSuit(card))
-//			{
-//				return true;
-//			}
-//		}
-//		//IF THE CARDS DO NOT WRAP OR ARE NOT THE SAME SUIT, CAN'T ADD THE CARD.
-//		return false;
-//	}
-	
+
+	//	@Override
+	//	public boolean isAddingCardLegal(Card card)
+	//	{
+	//		//WE ARE CHECKING TOP CARD AND COMPARING IT TO @PARAM CARD TO SEE IF IT WRAPS.
+	//		if(homecellPile.peek().canBuild(card) || homecellPile.peek().canWrap(card))
+	//		{
+	//			//WE ARE CHECKING TOP CARD AND COMPARING IT TO @PARAM CARD TO SEE IF ITS SAME SUIT.
+	//			if(homecellPile.peek().isSameSuit(card))
+	//			{
+	//				return true;
+	//			}
+	//		}
+	//		//IF THE CARDS DO NOT WRAP OR ARE NOT THE SAME SUIT, CAN'T ADD THE CARD.
+	//		return false;
+	//	}
+
 	/**
 	 * Only the card which is currently at the top of the homecell pile can be removed.
 	 * You cannot remove the last card in the homecell pile.
@@ -98,10 +100,10 @@ public class LittleSpiderHomecell extends Homecell {
 		{
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	/*
 	 * Gets a homecellPile, allows for testing
 	 * 
@@ -111,9 +113,9 @@ public class LittleSpiderHomecell extends Homecell {
 		return this.homecellPile;
 	}
 
-}
+} 
 
-
+ 
 
 
 
--- src/edu/buffalo/cse116/tests/littleSpider/LittleSpiderHomecellTest.java
+++ src/edu/buffalo/cse116/tests/littleSpider/LittleSpiderHomecellTest.java
@@ -16,15 +16,12 @@ import edu.buffalo.cse116.code.littleSpider.LittleSpiderTableau;
 
 public class LittleSpiderHomecellTest {
 
-	
-	
-		 
 	/*
-	 *  Tests if heart and diamond cards are being correctly added to the homecell piles. Also tests if isAddingCardIsLegal for heart and diamond card
+	 *  Tests if heart and diamond cards are being correctly added to the homecell piles. Also tests isAddingCardIsLegal for heart and diamond cards
+	 *  
+	 *  Heart and diamond cards can only be added to their corresponding homecell piles if their suit is the same and they build UP
 	 */
-	
-	
-	
+
 	@Test
 	public void canAddHeartsAndDiamonds() {
 		LittleSpiderHomecell LSHHearts = new LittleSpiderHomecell();
@@ -46,7 +43,7 @@ public class LittleSpiderHomecellTest {
 		LSHHearts.addCard(new Card(0,11));
 		System.out.println(LSHHearts.getHomecellPile().size() + " is the size of pile1 after attempt 3 \n");
 		assertEquals(7, LSHHearts.getHomecellPile().size());
-		
+
 		LSHDiamonds.getHomecellPile().push(new Card(1, 2));
 		LSHDiamonds.getHomecellPile().push(new Card(1, 5));
 		LSHDiamonds.getHomecellPile().push(new Card(1, 7));
@@ -65,94 +62,101 @@ public class LittleSpiderHomecellTest {
 		System.out.println(LSHDiamonds.getHomecellPile().size() + " is the size of pile2 after attempt 3 \n");
 		assertEquals(7, LSHDiamonds.getHomecellPile().size());
 	}
-	
-	//Unsure as to why this test keeps failing. 
-	
-		@Test
-		public void canAddClubsAndSpades() {
-			LittleSpiderHomecell LSHClubs = new LittleSpiderHomecell();
-			LittleSpiderHomecell LSHSpades = new LittleSpiderHomecell();
-			LSHClubs.getHomecellPile().push(new Card(0, 2));
-			LSHClubs.getHomecellPile().push(new Card(3, 11));
-			LSHClubs.getHomecellPile().push(new Card(0, 1));
-			LSHClubs.getHomecellPile().push(new Card(0, 6));
-			System.out.println(LSHClubs.getHomecellPile().lastElement().getRank() + " -this is the last element's rank");
-			System.out.println(LSHClubs.getHomecellPile().size() + " is the size of pile1 initially");
-			LSHClubs.addCard(new Card(0,5));
-			System.out.println(LSHClubs.getHomecellPile().size() + " is the size of pile1 after attempt 1");
-			assertEquals(5, LSHClubs.getHomecellPile().size());
-			LSHClubs.addCard(new Card(0,4));
-			System.out.println(LSHClubs.getHomecellPile().size() + " is the size of pile1 after attempt 2");
-			assertEquals(6, LSHClubs.getHomecellPile().size());
-			LSHClubs.addCard(new Card(3,3));
-			LSHClubs.addCard(new Card(0,3));
-			LSHClubs.addCard(new Card(0,8));
-			LSHClubs.addCard(new Card(0,11));
-			System.out.println(LSHClubs.getHomecellPile().size() + " is the size of pile1 after attempt 3 \n");
-			assertEquals(7, LSHClubs.getHomecellPile().size());
-			
-			LSHSpades.getHomecellPile().push(new Card(3, 2));
-			LSHSpades.getHomecellPile().push(new Card(3, 5));
-			LSHSpades.getHomecellPile().push(new Card(3, 7));
-			LSHSpades.getHomecellPile().push(new Card(3, 9));
-			System.out.println(LSHSpades.getHomecellPile().size() + " is the size of pile1 initially");
-			LSHSpades.addCard(new Card(3,8));
-			System.out.println(LSHSpades.getHomecellPile().size() + " is the size of pile1 after attempt 1");
-			assertEquals(5, LSHSpades.getHomecellPile().size());
-			LSHSpades.addCard(new Card(3,7));
-			System.out.println(LSHSpades.getHomecellPile().size() + " is the size of pile1 after attempt 2");
-			assertEquals(6, LSHSpades.getHomecellPile().size());
-			LSHSpades.addCard(new Card(1,12));
-			LSHSpades.addCard(new Card(3,6));
-			LSHSpades.addCard(new Card(3,11));
-			LSHSpades.addCard(new Card(0,11));
-			System.out.println(LSHSpades.getHomecellPile().size() + " is the size of pile1 after attempt 3 \n");
-			assertEquals(7, LSHSpades.getHomecellPile().size());
-			
-		}
 
-		
-//	}
-	
+	/*
+	 *  Tests if clubs and spades cards are being correctly added to the homecell piles. Also tests isAddingCardIsLegal for clubs and spade cards
+	 *   
+	 *  Club and spade cards can only be added to their corresponding homecell piles if their suit is the same and they build DOWN
+	 */
+
+	@Test
+	public void canAddClubsAndSpades() {
+		LittleSpiderHomecell LSHClubs = new LittleSpiderHomecell();
+		LittleSpiderHomecell LSHSpades = new LittleSpiderHomecell();
+		LSHClubs.getHomecellPile().push(new Card(0, 2));
+		LSHClubs.getHomecellPile().push(new Card(3, 11));
+		LSHClubs.getHomecellPile().push(new Card(0, 1));
+		LSHClubs.getHomecellPile().push(new Card(0, 6));
+		System.out.println(LSHClubs.getHomecellPile().lastElement().getRank() + " -this is the last element's rank");
+		System.out.println(LSHClubs.getHomecellPile().size() + " is the size of pile1 initially");
+		LSHClubs.addCard(new Card(0,5));
+		System.out.println(LSHClubs.getHomecellPile().size() + " is the size of pile1 after attempt 1");
+		assertEquals(5, LSHClubs.getHomecellPile().size());
+		LSHClubs.addCard(new Card(0,4));
+		System.out.println(LSHClubs.getHomecellPile().size() + " is the size of pile1 after attempt 2");
+		assertEquals(6, LSHClubs.getHomecellPile().size());
+		LSHClubs.addCard(new Card(3,3));
+		LSHClubs.addCard(new Card(0,3));
+		LSHClubs.addCard(new Card(0,8));
+		LSHClubs.addCard(new Card(0,11));
+		System.out.println(LSHClubs.getHomecellPile().size() + " is the size of pile1 after attempt 3 \n");
+		assertEquals(7, LSHClubs.getHomecellPile().size());
+
+		LSHSpades.getHomecellPile().push(new Card(3, 2));
+		LSHSpades.getHomecellPile().push(new Card(3, 5));
+		LSHSpades.getHomecellPile().push(new Card(3, 7));
+		LSHSpades.getHomecellPile().push(new Card(3, 9));
+		System.out.println(LSHSpades.getHomecellPile().size() + " is the size of pile1 initially");
+		LSHSpades.addCard(new Card(3,8));
+		System.out.println(LSHSpades.getHomecellPile().size() + " is the size of pile1 after attempt 1");
+		assertEquals(5, LSHSpades.getHomecellPile().size());
+		LSHSpades.addCard(new Card(3,7));
+		System.out.println(LSHSpades.getHomecellPile().size() + " is the size of pile1 after attempt 2");
+		assertEquals(6, LSHSpades.getHomecellPile().size());
+		LSHSpades.addCard(new Card(1,12));
+		LSHSpades.addCard(new Card(3,6));
+		LSHSpades.addCard(new Card(3,11));
+		LSHSpades.addCard(new Card(0,11));
+		System.out.println(LSHSpades.getHomecellPile().size() + " is the size of pile1 after attempt 3 \n");
+		assertEquals(7, LSHSpades.getHomecellPile().size());
+
+	}
+
+	/*
+	 * Tests if cards can be added to an empty "tableau" pile (They CANNOT)
+	 */
+
+	@Test
+	public void cantAddToEmptyStack() {
+		LittleSpiderHomecell LHS = new LittleSpiderHomecell();
+		System.out.println(LHS.getHomecellPile().size() + " is the size before");
+		LHS.addCard(new Card(2, 1));
+		System.out.println(LHS.getHomecellPile().size() + " is the size after");
+		assertEquals(0, LHS.getHomecellPile().size());
+	}
+
 	/*
 	 * Tests if removing cards from the homecell piles is legal. 
 	 * Also tests if ifRemovingCardIsLegal works.
 	 * 
-	 * 
+	 * Removing is only legal if there is MORE than 1 card in the homecell pile
 	 */
-	
+
 	@Test
 	public void canRemoveCard() {
-	Deck deck = new Deck();
-	LittleSpiderHomecell LSH = new LittleSpiderHomecell();
-	LittleSpiderTableau LST = new LittleSpiderTableau(deck);
-	LST.getPile().push(new Card (2,10));
-	LSH.getHomecellPile().push(new Card(2, 3));
-	LSH.getHomecellPile().push(new Card(2, 5));
-	LSH.getHomecellPile().push(new Card(3, 12));
-	LSH.getHomecellPile().push(new Card(1, 8));
-	LSH.getHomecellPile().push(new Card(2, 8));
-//	System.out.println(LSH.getHomecellPile().size() + " size before");
-//	System.out.println(LSH.getHomecellPile().lastElement().getSuit() + " " + LSH.getHomecellPile().lastElement().getRank());
-	LSH.removeCard();
-	assertEquals(4, LSH.getHomecellPile().size());
-	LSH.removeCard();
-	assertEquals(3, LSH.getHomecellPile().size());
-	LSH.removeCard();
-	assertEquals(2, LSH.getHomecellPile().size());
-	LSH.removeCard();
-	assertEquals(1, LSH.getHomecellPile().size());
-	LSH.removeCard();
-	assertEquals(1, LSH.getHomecellPile().size());
-//	System.out.println(LSH.getHomecellPile().lastElement().getSuit() + " " + LSH.getHomecellPile().lastElement().getRank());
-//	System.out.println(LSH.getHomecellPile().size() + " size after");
-//	
-	
+		Deck deck = new Deck();
+		LittleSpiderHomecell LSH = new LittleSpiderHomecell();
+		LittleSpiderTableau LST = new LittleSpiderTableau(deck);
+		LST.getPile().push(new Card (2,10));
+		LSH.getHomecellPile().push(new Card(2, 3));
+		LSH.getHomecellPile().push(new Card(2, 5));
+		LSH.getHomecellPile().push(new Card(3, 12));
+		LSH.getHomecellPile().push(new Card(1, 8));
+		LSH.getHomecellPile().push(new Card(2, 8));
+		System.out.println(LSH.getHomecellPile().size() + " size before");
+		System.out.println(LSH.getHomecellPile().lastElement().getSuit() + " " + LSH.getHomecellPile().lastElement().getRank());
+		LSH.removeCard();
+		assertEquals(4, LSH.getHomecellPile().size());
+		LSH.removeCard();
+		assertEquals(3, LSH.getHomecellPile().size());
+		LSH.removeCard();
+		assertEquals(2, LSH.getHomecellPile().size());
+		LSH.removeCard();
+		assertEquals(1, LSH.getHomecellPile().size());
+		LSH.removeCard();
+		assertEquals(1, LSH.getHomecellPile().size());
+		System.out.println(LSH.getHomecellPile().lastElement().getSuit() + " " + LSH.getHomecellPile().lastElement().getRank());
+		System.out.println(LSH.getHomecellPile().size() + " size after");
 	}
-	
-	
-	
-	
-	
-	
+
 }

0e5706a21eaa7a6dac7ab1e01a543342cc07fdc3
Andrew Quinonez <darthaq@gmail.com>
2018-09-30 16:14:53 -0400

    Fixed the littleSpiderGame and test class for it

--- src/edu/buffalo/cse116/code/littleSpider/LittleSpiderGame.java
+++ src/edu/buffalo/cse116/code/littleSpider/LittleSpiderGame.java
@@ -32,32 +32,48 @@ public class LittleSpiderGame
 		this.gameTableauPiles = new ArrayList<LittleSpiderTableau>();
 		this.gameHomecellPiles = new ArrayList<LittleSpiderHomecell>();
 		
-		for(int i=0; i<this.gameDeck.getDeck().size(); i++)
-		{
-			Card currentCard = this.gameDeck.getDeck().get(i);
 			
-			if(currentCard.getRank() == 0)
-			{
-				if(currentCard.getSuit() == 1 || currentCard.getSuit() == 2)
-				{
-					LittleSpiderHomecell newHomecellPile = new LittleSpiderHomecell();
-					newHomecellPile.getHomecellPile().add(currentCard);
-					this.gameHomecellPiles.add(newHomecellPile);
-					this.gameDeck.getDeck().remove(i);
-				}
-			}
-			
-			if(currentCard.getRank() == 12)
-			{
-				if(currentCard.getSuit() == 0 || currentCard.getSuit() == 3)
-				{
-					LittleSpiderHomecell newHomecellPile = new LittleSpiderHomecell();
-					newHomecellPile.getHomecellPile().add(currentCard);
-					this.gameHomecellPiles.add(newHomecellPile);
-					this.gameDeck.getDeck().remove(i);
-				}
-			}
+		//Holds the four cards we'll need for the homecell piles
+		Card aDiamonds = gameDeck.getDeck().get(13);
+		Card aHearts = gameDeck.getDeck().get(26);
+		Card kClubs = gameDeck.getDeck().get(12);
+		Card kSpades = gameDeck.getDeck().get(51);
+				
+		//Once we have references to the cards we need, from the deck class we'll remove these cards
+		gameDeck.getDeck().remove(51);
+		gameDeck.getDeck().remove(26);
+		gameDeck.getDeck().remove(13);
+		gameDeck.getDeck().remove(12);
+				
+		
+		LittleSpiderHomecell hcp1 = new LittleSpiderHomecell();		
+		LittleSpiderHomecell hcp2 = new LittleSpiderHomecell();		
+		LittleSpiderHomecell hcp3 = new LittleSpiderHomecell();		
+		LittleSpiderHomecell hcp4 = new LittleSpiderHomecell();		
+		
+		//Now take the references for the cards and add it to the homecell piles
+		hcp1.getHomecellPile().add(aDiamonds);
+		hcp2.getHomecellPile().add(aHearts);
+		hcp3.getHomecellPile().add(kClubs);
+		hcp4.getHomecellPile().add(kSpades);
+		
+		//Now we add our homecell piles to the list of homecell piles
+		gameHomecellPiles.add(hcp1);
+		gameHomecellPiles.add(hcp2);
+		gameHomecellPiles.add(hcp3);
+		gameHomecellPiles.add(hcp4);
+		
+		//Now with the remaining 48 cards, we'll shuffle
+		Collections.shuffle(gameDeck.getDeck());
+		
+		
+		//Now take these 48 cards and create 8 tableau piles where each pile will get 6 cards
+		for(int i = 0; i < 8; i++) {	
+			LittleSpiderTableau lst = new LittleSpiderTableau(gameDeck);
+			gameTableauPiles.add(lst);
 		}
+		
+		
 	}
 	
 	/*
@@ -75,6 +91,10 @@ public class LittleSpiderGame
 
 	public ArrayList<LittleSpiderHomecell> getHomecellPiles(){
 		return this.gameHomecellPiles;
+	}
+	
+	public ArrayList<LittleSpiderTableau> getTableauPiles(){
+		return this.gameTableauPiles;
 	}
 
 	
--- src/edu/buffalo/cse116/tests/littleSpider/LittleSpiderGameTest.java
+++ src/edu/buffalo/cse116/tests/littleSpider/LittleSpiderGameTest.java
@@ -4,28 +4,54 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import edu.buffalo.cse116.code.Card;
 import edu.buffalo.cse116.code.littleSpider.LittleSpiderGame;
 
 public class LittleSpiderGameTest {
 
-	//Both tests fail every so often. It should not, I have done extensive testing to see what cards are being instantiated
-	//and it seems like the right cards are being instantiated but one of the cards is not always being deleted
-	 
+	
+	
+	
+	
 	@Test
-	public void testDeckSize() {
-		LittleSpiderGame LTG = new LittleSpiderGame();
-		System.out.print(LTG.getCurrentDeck().getDeck().size() + " this is the size of the deck");
-		assertEquals(48, LTG.getCurrentDeck().getDeck().size()); 
+	public void testForCorrectHomecell() {
+		LittleSpiderGame lsg = new LittleSpiderGame();
+		assertEquals(4, lsg.getHomecellPiles().size());
 		
+		//Ace of diamonds
+		Card aDiamonds = new Card(1, 0);
+		//Ace of Hearts
+		Card aHearts = new Card(2, 0);
+		//King of Clubs
+		Card kClubs = new Card(0, 12);
+		//King of Spades
+		Card kSpades = new Card(3, 12);
+		
+		
+		//Check if the first homecell pile only contains ace of diamonds
+		assertEquals(aDiamonds.getSuit(), lsg.getHomecellPiles().get(0).getHomecellPile().get(0).getSuit());
+		assertEquals(aDiamonds.getRank(), lsg.getHomecellPiles().get(0).getHomecellPile().get(0).getRank());
+		//Check if the 2nd homecell pile only contains ace of hearts
+		assertEquals(aHearts.getSuit(), lsg.getHomecellPiles().get(1).getHomecellPile().get(0).getSuit());
+		assertEquals(aHearts.getRank(), lsg.getHomecellPiles().get(1).getHomecellPile().get(0).getRank());
+		//Check if the 3rd homecell pile only contains king of clubs
+		assertEquals(kClubs.getSuit(), lsg.getHomecellPiles().get(2).getHomecellPile().get(0).getSuit());
+		assertEquals(kClubs.getRank(), lsg.getHomecellPiles().get(2).getHomecellPile().get(0).getRank());
+		//Check if the 4th homecell pile only contains king of spades
+		assertEquals(kSpades.getSuit(), lsg.getHomecellPiles().get(3).getHomecellPile().get(0).getSuit());
+		assertEquals(kSpades.getRank(), lsg.getHomecellPiles().get(3).getHomecellPile().get(0).getRank());
 	}
 	
 	@Test
-	public void FourHomecellsInstantiated() {
-		LittleSpiderGame LTG = new LittleSpiderGame();
-		System.out.println(LTG.getHomecellPiles().size() + " this is the amount of homecell piles");
-		assertEquals(4, LTG.getHomecellPiles().size());
+	public void testForEightTableauPiles() {
+		LittleSpiderGame lts = new LittleSpiderGame();
+		assertEquals(8, lts.getTableauPiles().size());
 		
+		for(int i = 0; i < lts.getTableauPiles().size();i++) {
+			assertEquals(6, lts.getTableauPiles().get(i).getPileSize());
+		}
 	}
-
+	
+	
 	
 }

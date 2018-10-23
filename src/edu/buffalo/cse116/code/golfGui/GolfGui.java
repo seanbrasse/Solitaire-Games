2f5696bde96c35a497a3bef4a285c4cd60642284
Andrew Quinonez <darthaq@gmail.com>
2018-10-23 15:47:05 -0400

    Class name changes

--- src/edu/buffalo/cse116/code/golfGui/GolfGui.java
+++ src/edu/buffalo/cse116/code/golfGui/GolfGui.java
@@ -2,10 +2,10 @@ package edu.buffalo.cse116.code.golfGui;
 
 /**
  * Creates the necessary objects for the game. This includes the tableau, homecell and stock pile. Then
- * instantiates the logic classes for the game.
+ * instantiates the logic classes for the game. I.e. the base class to place all Golf related things.
  * @author Andrew Quinonez
  *
  */
-public class golfGui {
+public class GolfGui {
 
 }

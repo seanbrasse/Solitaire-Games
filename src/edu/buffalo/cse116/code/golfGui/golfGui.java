13fb46f1e063aeeaac68a8df2dea6d6970b33a4c
Andrew Quinonez <darthaq@gmail.com>
2018-10-23 15:44:48 -0400

    Create the base class for our Golf game

---
+++ src/edu/buffalo/cse116/code/golfGui/golfGui.java
@@ -0,0 +1,11 @@
+package edu.buffalo.cse116.code.golfGui;
+
+/**
+ * Creates the necessary objects for the game. This includes the tableau, homecell and stock pile. Then
+ * instantiates the logic classes for the game.
+ * @author Andrew Quinonez
+ *
+ */
+public class golfGui {
+
+}

db624587a0f197d90c0b52de4f0e4eda795179eb
William Nicholson <williamnicholson99@gmail.com>
2018-10-28 16:00:06 -0400

    comments for golf homecell listener

--- src/edu/buffalo/cse116/code/golfGUI/GolfHomecellListener.java
+++ src/edu/buffalo/cse116/code/golfGUI/GolfHomecellListener.java
@@ -3,15 +3,32 @@ package edu.buffalo.cse116.code.golfGUI;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+/**
+ * Makes changes to model through
+ * interactions with homecell piles.
+ * 
+ * @author William F. Nicholson
+ *
+ */
 public class GolfHomecellListener implements MouseListener
 {
+	/**
+	 * View the controller will manuipulate
+	 */
 	private GolfLayeredPanel pane;
 	
+	/**
+	 * Allow mouse listener to get values from game.
+	 * @param pane
+	 */
 	public GolfHomecellListener(GolfLayeredPanel pane)
 	{
 		this.pane = pane;
 	}
 	
+	/**
+	 * On mouse click, tell the user they can't do anything to the homecell pile.
+	 */
 	@Override
 	public void mouseClicked(MouseEvent e)
 	{

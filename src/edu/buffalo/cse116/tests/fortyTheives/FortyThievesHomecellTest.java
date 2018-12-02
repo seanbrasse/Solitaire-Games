32b9e8569277a64dd93a045cae2fb5ceaf8f508d
Wilmer <Wilmer@10.84.23.30>
2018-12-01 23:40:29 -0500

    homecell initial test

--- src/edu/buffalo/cse116/tests/fortyTheives/FortyThievesHomecellTest.java
+++ src/edu/buffalo/cse116/tests/fortyTheives/FortyThievesHomecellTest.java
@@ -5,11 +5,18 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import edu.buffalo.cse116.code.fortyThieves.FortyThievesGame;
+import edu.buffalo.cse116.code.fortyThieves.FortyThievesHomecell;
 
 public class FortyThievesHomecellTest {
 	@Test
 	public void InitialHomecellPiles(){
 		FortyThievesGame InitialPileSize = new FortyThievesGame();
 		assertEquals(1, InitialPileSize.getGameHomecellPiles().size());
+	}
+	
+	@Test
+	public void IsAddingLegal() {
+		FortyThievesHomecell gg = new FortyThievesHomecell();
+		
 	}
 }
